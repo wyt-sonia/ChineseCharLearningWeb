5c5774ca7f63e52caa7ad40bdaba05c0aade212c
wyt-sonia <e0365980@u.nus.edu>
2021-05-16 22:47:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
   $('#startBtn').click(function() {
-    $('.cover_container').fadeOut('5000');
-    $('#container').css('background-image', "url('/css/images/css.jpg')");
+    $('body').fadeOut('5000');
+    $('body').fadeOut(500, function() {
+      $('body').css({
+        background:
+          "url('https://raw.githubusercontent.com/wyt-sonia/ChineseCharLearningWeb/master/asset/Image/chars5.jpg')",
+        'background-size': 'cover'
+      });
+      $('body').fadeIn(1000);
+    });
   });
   $('#backBtn').click(function() {
     $('.cover_container').fadeIn('5000');
+    $('body').fadeOut(500, function() {
+      $('body').css({
+        background:
+          "url('https://raw.githubusercontent.com/wyt-sonia/ChineseCharLearningWeb/master/asset/Image/chars7.jpg')",
+        'background-size': 'cover'
+      });
+      $('body').fadeIn(1000);
+    });
   });
 });
