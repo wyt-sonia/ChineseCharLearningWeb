843cff3da183f488f814610c1a1a3aa516e73681
wyt-sonia <e0365980@u.nus.edu>
2021-05-16 23:34:58 +0800

    1 file modified

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
 $(document).ready(function() {
+  var isGaming = false;
+
   $('#startBtn').click(function() {
-    $('body').fadeOut('5000');
-    $('body').fadeOut(500, function() {
-      $('body').css({
-        background:
-          "url('https://raw.githubusercontent.com/wyt-sonia/ChineseCharLearningWeb/master/asset/Image/chars5.jpg')",
-        'background-size': 'cover'
-      });
-      $('body').fadeIn(1000);
-    });
+    toggleContents(isGaming);
+    isGaming = !isGaming;
   });
-  $('#backBtn').click(function() {
-    $('.cover_container').fadeIn('5000');
-    $('body').fadeOut(500, function() {
-      $('body').css({
-        background:
-          "url('https://raw.githubusercontent.com/wyt-sonia/ChineseCharLearningWeb/master/asset/Image/chars7.jpg')",
-        'background-size': 'cover'
+
+  function toggleContents(isGaming) {
+    if (!isGaming) {
+      $('.cover_container').fadeIn('5000');
+
+      $('#startBtn').fadeOut('5000', function() {
+        $('#startBtn').html('Back to Home');
+        $('#startBtn').fadeIn();
       });
-      $('body').fadeIn(1000);
-    });
-  });
+
+      $('body').fadeOut('5000');
+      $('body').fadeOut(500, function() {
+        $('body').css({
+          background:
+            "url('https://raw.githubusercontent.com/wyt-sonia/ChineseCharLearningWeb/master/asset/Image/chars5.jpg')",
+          'background-size': 'cover'
+        });
+        $('body').fadeIn(1000);
+
+        $('#cover_container').fadeOut(1);
+        $('#game_container').fadeIn(1000);
+      });
+    } else {
+      $('.cover_container').fadeIn('5000');
+
+      $('#startBtn').fadeOut('5000', function() {
+        $('#startBtn').html('Start the game');
+        $('#startBtn').fadeIn();
+      });
+
+      $('body').fadeOut(500, function() {
+        $('body').css({
+          background:
+            "url('https://raw.githubusercontent.com/wyt-sonia/ChineseCharLearningWeb/master/asset/Image/chars7.jpg')",
+          'background-size': 'cover'
+        });
+        $('body').fadeIn(1000);
+        $('#game_container').fadeOut(1);
+        $('#cover_container').fadeIn(1000);
+      });
+    }
+  }
 });
