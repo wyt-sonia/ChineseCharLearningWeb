878f49d0e8cb6cb183a69fca2103363647cb0e05
wyt-sonia <wyt.sonia@gmail.com>
2021-05-26 23:19:56 +0800

    update

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ $(document).ready(function() {
     toggleContents(isGaming);
     initST(0);
     initParts(0);
-    dismissHintContent()
+    dismissHintContent();
 
     isGaming = !isGaming;
   });
